e69b3646a97c5a0fc862e14a98811965e9515950
Robert Papoyan <robertpapoyan96@gmail.com>
2020-06-11 22:19:09 +0400

    Create User UI and the way to it is created

---
+++ app/src/main/java/com/example/chatx/CreateUserActivity.kt
@@ -0,0 +1,27 @@
+package com.example.chatx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatarImgClicked(view: View){
+
+    }
+
+    fun generateBackgroundColourBtnClicked(view: View) {
+
+    }
+
+    fun createUserBtnClicked(view: View) {
+
+    }
+
+
+}
